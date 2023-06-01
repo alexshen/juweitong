a5d4161363bf67948135dcdffd358897deeaefb7
Alex Shen <swlsww@hotmail.com>
2023-06-02 06:50:33 +0800

    remove NullClientsDAO

--- cmd/atom-server/dal/null_dao.go
+++ cmd/atom-server/dal/null_dao.go
@@ -1,21 +1,5 @@
 package dal
 
-import "github.com/google/uuid"
-
-type NullClientsDAO struct{}
-
-func (o NullClientsDAO) Has(id string) (bool, error) {
-	return true, nil
-}
-
-func (o NullClientsDAO) Create() (string, error) {
-	id, err := uuid.NewRandom()
-	if err != nil {
-		return "", err
-	}
-	return id.String(), nil
-}
-
 type NullLikedPostsDAO struct{}
 
 func (o NullLikedPostsDAO) Has(record LikedPost) (bool, error) {
