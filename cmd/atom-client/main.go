ff981a25bbae1ea167061e264a19c225bbb580f2
Alex Shen <swlsww@hotmail.com>
2023-06-03 08:25:57 +0800

    always switch communities

--- cmd/atom-client/main.go
+++ cmd/atom-client/main.go
@@ -4,17 +4,8 @@ import (
 	"log"
 
 	"github.com/alexshen/juweitong/atom"
-	"github.com/samber/lo"
 	"github.com/skratchdot/open-golang/open"
 )
-
-// rotateCopy rotates the slice and returns a new copy that begins with s[i:]
-// followed by s[0:i]
-func rotateCopy[S ~[]E, E any](s S, i int) S {
-	res := make(S, 0, len(s))
-	res = append(res, s[i:]...)
-	return append(res, s[0:i]...)
-}
 
 func main() {
 	client := atom.NewClient(atom.NullLikedPostsHistory{})
@@ -29,15 +20,11 @@ func main() {
 	log.Printf("QR Code: %s\n", url)
 	open.Run(url)
 	<-loggedIn
-	communities := rotateCopy(client.Communities(), client.CurrentCommunityIndex())
-	for i, comm := range communities {
-		if i != 0 {
-			log.Printf("Switching to community: %s", comm.Name)
-			j := lo.IndexOf(client.Communities(), comm)
-			if err := client.SetCurrentCommunity(j); err != nil {
-				log.Print(err)
-				continue
-			}
+	for _, comm := range client.Communities() {
+		log.Printf("Switching to community: %s, %s", comm.Name, comm.MemberId)
+		if err := client.SetCurrentCommunityById(comm.MemberId); err != nil {
+			log.Print(err)
+			continue
 		}
 		log.Printf("Visiting community: %s", comm.Name)
 		log.Printf("Liked notices: %d", client.LikeNotices(1))
